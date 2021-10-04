5ddaf2bdfb57b6a35d292f97a27360c70181a513
225junior <35270982+225junior@users.noreply.github.com>
2021-10-04 14:41:02 +0000

    premiere route user

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
+const mockUserData = [
+    {'name':'Mark'},
+    {'name':'Jill'}
+]
+
+
+
+app.get('/users',function (req,res) {
+    res.json({
+        success:true,
+        message:'Liste des utilisateurs',
+        Users:mockUserData
+    })
+})
+
+
 
 app.listen(8000,function(){
     console.log("Server is running")
